48b5fb892521a58bdebab8362ef28183a6dae583
sparrowal3amre-design <sparrowal3amre@gmail.com>
2026-05-14 10:49:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 from flask import Flask, render_template, request, send_file
-import os
 from pdf2docx import Converter
-from docx2pdf import convert
 from werkzeug.utils import secure_filename
+import subprocess
+import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/convert', methods=['POST'])
+@app.route("/convert", methods=["POST"])
 def convert_file():
-    if 'file' not in request.files:
-        return 'No file uploaded'
 
-    file = request.files['file']
-    mode = request.form.get('mode')
+    if "file" not in request.files:
+        return "No file uploaded"
 
-    if file.filename == '':
-        return 'No selected file'
+    file = request.files["file"]
+    mode = request.form.get("mode")
+
+    if file.filename == "":
+        return "No selected file"
 
     filename = secure_filename(file.filename)
     filepath = os.path.join(UPLOAD_FOLDER, filename)
+
     file.save(filepath)
 
-    if mode == 'pdf_to_word':
-        output_path = filepath.replace('.pdf', '.docx')
+    # PDF → WORD
+    if mode == "pdf_to_word":
+
+        output_path = filepath.replace(".pdf", ".docx")
 
         cv = Converter(filepath)
         cv.convert(output_path)
@@ -37,14 +41,30 @@ def convert_file():
 
         return send_file(output_path, as_attachment=True)
 
-    elif mode == 'word_to_pdf':
-        output_path = filepath.replace('.docx', '.pdf')
+    # WORD → PDF
+    elif mode == "word_to_pdf":
 
-        convert(filepath, output_path)
+        output_path = filepath.replace(".docx", ".pdf")
+
+        subprocess.run([
+            "libreoffice",
+            "--headless",
+            "--convert-to",
+            "pdf",
+            filepath,
+            "--outdir",
+            UPLOAD_FOLDER
+        ])
 
         return send_file(output_path, as_attachment=True)
 
-    return 'Invalid mode'
+    return "Invalid mode"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
